40d5f6e33a3033fbaa1a3a9d526f5d8340d34100
Yavuz Selim Demiryılmaz <ysdemiryilmaz@gmail.com>
2023-09-04 02:24:26 +0300

    add mutable variable

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,11 @@ fn main() {
     let x: i32 = 5;
     let _y: i32; // uninitialized, but no warnings because of the underscore before variable name
     assert_eq!(x, 5); // if not equal, program will panic
-    println!("Success!")
+    println!("Success! 1");
+
+    let mut a = 1; // mutable variable
+    a += 2;
+
+    assert_eq!(a, 3);
+    println!("Success! 2");
 }
