dd74afa4fefe55145db3989efcef997a00ac346f
Yavuz Selim Demiryılmaz <ysdemiryilmaz@gmail.com>
2023-09-04 02:19:32 +0300

    add uninitialized variable without warnings

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("{:#?}", now);
 
     let x: i32 = 5;
+    let _y: i32; // uninitialized, but no warnings because of the underscore before variable name
     assert_eq!(x, 5); // if not equal, program will panic
     println!("Success!")
 }
