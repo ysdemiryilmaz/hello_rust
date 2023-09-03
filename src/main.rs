682d10def314fe302f6111ec8b387b5581146d58
Yavuz Selim Demiryılmaz <ysdemiryilmaz@gmail.com>
2023-09-04 02:14:01 +0300

    use "assert_eq!"

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ fn main() {
     println!("Hello, Cargo!");
     let now = SystemTime::now();
     println!("{:#?}", now);
+
+    let x: i32 = 5;
+    assert_eq!(x, 5); // if not, program will panic
+    println!("Success!")
 }
